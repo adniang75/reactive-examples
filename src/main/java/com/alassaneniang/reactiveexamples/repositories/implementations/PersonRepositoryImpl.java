e81611d96888890deab828f923cf10ef7677bfe3
Alassane NIANG <adniang75@gmail.com>
2022-01-22 05:24:50 +0100

    Setting up initial functionality to get ready to start

--- src/main/java/com/alassaneniang/reactiveexamples/repositories/implementations/PersonRepositoryImpl.java
+++ src/main/java/com/alassaneniang/reactiveexamples/repositories/implementations/PersonRepositoryImpl.java
@@ -6,13 +6,22 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public class PersonRepositoryImpl implements PersonRepository {
+
+    // emulating a Person repository
+    Person alassane = new Person( 1, "Alassane", "Niang" );
+    Person elon = new Person( 2, "Elon", "Musk" );
+    Person jeff = new Person( 3, "Jeff", "Bezos" );
+    Person barack = new Person( 4, "Barack", "Obama" );
+
     @Override
     public Mono<Person> getById ( Integer id ) {
-        return null;
+        // publisher of zero or one element
+        return Mono.just( alassane );
     }
 
     @Override
     public Flux<Person> findAll () {
-        return null;
+        // publisher of zero or more elements
+        return Flux.just( alassane, elon, jeff, barack );
     }
 }
